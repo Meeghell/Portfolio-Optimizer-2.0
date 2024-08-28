558544bd3ff4399e107626a92c3a2241948fbac9
jillstillens <jillstill@rocketmail.com>
2024-08-28 12:28:19 -0700

    updated form to display images; add sentiment

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
 import streamlit as st
-import io
-import base64
 import json
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
+import os
+import glob
+#python imaging library, to display PNG files
+from PIL import Image
 
 #initialize session state variables
-if 'num_fields' not in st.session_state:
-    st.session_state.num_fields = 1
-if 'symbols' not in st.session_state:
-    st.session_state.symbols = ['']
+def initialize():
+    if 'num_fields' not in st.session_state:
+        st.session_state.num_fields = 1
+    if 'symbols' not in st.session_state:
+        st.session_state.symbols = ['']
+
+# Delete files from previous run
+def pre_cleanup():
+    # clean up existing image files
+    files = glob.glob('/images/*')
+    for f in files:
+        os.remove(f)
+    # clean up existing data files
+    files = glob.glob('/data/*')
+    for f in files:
+        os.remove(f)
+    # clean up symbols and pickle filenames files:
+    if os.path.isfile("symbols.json"):
+        os.remove("symbols.json")
+    if os.path.isfile("pk_filenames.json"):
+        os.remove("pk_filenames.json")
 
 #function to allow entry of another stock symbol    
 def add_symbol():
@@ -24,8 +43,10 @@ def handle_form_submission():
     with open('symbols.json','w') as f:
         json.dump(entered_symbols,f)
     #invoke the controller notebook
-    execute_notebook('controller.ipynb')
-    
+    with st.spinner(text="Please wait - gathering and processing data"):
+        execute_notebook('controller.ipynb')
+    display_results()
+
 #function to invoke a jupyter notebook    
 def execute_notebook(notebook_name):
     #load notebook
@@ -35,7 +56,32 @@ def execute_notebook(notebook_name):
     ep = ExecutePreprocessor(time=600,kernal_name='python3')
     # execute notebook
     ep.preprocess(notebook,{'metadata':{'path':'./'}})
+
+# Upon completion of form submission processing, display resulting images
+def display_results():
+    # Define the path to images folder
+    folder_path = 'images'
+
+    # Get a list of all files in the folder
+    files = os.listdir(folder_path)
+
+    # Filter out only PNG files
+    png_files = [file for file in files if file.lower().endswith('.png')]
+
+    st.subheader("Results")
+    # Display each PNG file
+    for file_name in png_files:
+        # Construct full file path
+        file_path = os.path.join(folder_path, file_name)
+        # Diplay image on form
+        image = Image.open(file_path)
+        st.image(image, use_column_width=True)
     
+
+# Main processing
+initialize()
+pre_cleanup()
+
 # create form    
 with st.form(key='portfolio_form'):
      st.subheader("Portfolio Optimizer 2.0")
@@ -43,7 +89,9 @@ with st.form(key='portfolio_form'):
      col1, col2, col3, col4 = st.columns(4)
      for i in range(st.session_state.num_fields):
          with col1:
-            st.session_state.symbols[i] = st.text_input(f'Stock Symbol {i+1}',value=st.session_state.symbols[i], key=f'symbol_{i}')
+            st.session_state.symbols[i] = st.text_input(f'Stock Symbol {i+1}',value=st.session_state.symbols[i], key=f'symbol_{i}').upper()
+            # make symbols upper case
+            # st.session_state.symbols[i] = current_value.upper()
      add_button = st.form_submit_button('Add Symbol')
      submit_button  = st.form_submit_button('Optimize!')
      
@@ -55,5 +103,7 @@ with st.form(key='portfolio_form'):
      #if user is done entering symbols, and now wants to optimize the portfolio
      if submit_button:
         handle_form_submission()
+
+
 
     
